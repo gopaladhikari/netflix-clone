13bffa5891b1320657602b27e1366f3ce94862b2
Gopal Adhikari <gopuadks@gmail.com>
2023-06-07 15:39:17 +0545

    Completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Navbar.css";
 
 function Navbar() {
-  return <nav></nav>;
+  const [handleShow, setHandleShow] = useState(false);
+
+  useEffect(() => {
+    window.onscroll = () => {
+      if (window.scrollY > 100) setHandleShow(true);
+      else setHandleShow(false);
+    };
+  }, [handleShow]);
+
+  return (
+    <header className={`navbar ${handleShow && " nav_black"} `}>
+      <nav>
+        <img src="/logo.png" className="logo" alt="logo" />
+      </nav>
+    </header>
+  );
 }
 
 export default Navbar;
--- src/components/Rows.jsx
+++ src/components/Rows.jsx
@@ -1,10 +1,21 @@
 import React, { useEffect, useState } from "react";
 import instance from "../axios";
+import Youtube from "react-youtube";
+import movieTrailer from "movie-trailer";
 import "./Rows.css";
 
 function Rows({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+
   const baseUrl = "https://image.tmdb.org/t/p/original/";
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
 
   const fetchData = async () => {
     const response = await instance.get(fetchUrl);
@@ -16,6 +27,18 @@ function Rows({ title, fetchUrl, isLargeRow }) {
     fetchData();
   }, [fetchUrl]);
 
+  const handleClick = (movie) => {
+    if (trailerUrl) setTrailerUrl("");
+    else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
   return (
     <div className="row">
       <h2 style={{ marginBottom: "1rem" }}> {title} </h2>
@@ -23,6 +46,7 @@ function Rows({ title, fetchUrl, isLargeRow }) {
         {movies.map((movie) => {
           return (
             <img
+              onClick={() => handleClick(movie)}
               key={movie.id}
               className={`row_poster ${isLargeRow && "row_posterLarge"} `}
               src={`${baseUrl}${
@@ -33,6 +57,7 @@ function Rows({ title, fetchUrl, isLargeRow }) {
           );
         })}
       </div>
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
